ed98f279b325b2c5b9551c35fe5fe074007349bc
Heath Blandford <heathblandford@MacBook-Pro.local>
2020-07-11 06:15:19 -0400

    add script to update and run

---
+++ update_n_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+imageName=xx:discord-bot
+containerName=thirsty_ride
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p 5000:5000 --name $containerName $imageName
